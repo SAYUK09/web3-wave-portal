602ec7af57e7e96315c54e97cbd321be363f3190
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-11-26 12:21:59 +0530

    fix: contract address error fixed

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,25 +1,44 @@
-const main = async () => {
-  const [deployer] = await hre.ethers.getSigners();
-  const accountBalance = await deployer.getBalance();
+// const main = async () => {
+//   const [deployer] = await hre.ethers.getSigners();
+//   const accountBalance = await deployer.getBalance();
 
-  console.log("Deploying contracts with account: ", deployer.address);
-  console.log("Account balance: ", accountBalance.toString());
+//   console.log("Deploying contracts with account: ", deployer.address);
+//   console.log("Account balance: ", accountBalance.toString());
 
-  const Token = await hre.ethers.getContractFactory("WavePortal");
-  const portal = await Token.deploy();
-  await portal.deployed();
+//   const Token = await hre.ethers.getContractFactory("WavePortal");
+//   const portal = await Token.deploy();
+//   await portal.deployed();
 
-  console.log("WavePortal address: ", portal.address);
-};
+//   console.log("WavePortal address: ", portal.address);
+// };
 
-const runMain = async () => {
-  try {
-    await main();
-    process.exit(0);
-  } catch (error) {
-    console.error(error);
+async function main() {
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  // const waveContract = await waveContractFactory.deploy({
+  //   value: ethers.utils.parseEther("0.1"),
+  // });
+  // await waveContract.deployed();
+  // console.log(`WavePortal address: ${waveContract.address}`);
+
+  const waveContract = await waveContractFactory.deploy();
+  console.log(`WavePortal address: ${waveContract.address}`);
+}
+
+main()
+  .then((r) => process.exit(0))
+  .catch((err) => {
+    console.log(err);
     process.exit(1);
-  }
-};
+  });
 
-runMain();
+// const runMain = async () => {
+//   try {
+//     await main();
+//     process.exit(0);
+//   } catch (error) {
+//     console.error(error);
+//     process.exit(1);
+//   }
+// };
+
+// runMain();
